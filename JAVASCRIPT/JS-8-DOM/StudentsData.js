f010f4cb710eb063d731a8bde2823a80b0c66733
leelavathikm1 <leelavathikm1@gmail.com>
2023-04-19 19:51:40 +0530

    dom1

---
+++ JAVASCRIPT/JS-8-DOM/StudentsData.js
@@ -0,0 +1,44 @@
+var students = [
+  {
+    name: "Leela",
+    age: 40,
+    plase: "BLR",
+    mobile: 776693843,
+  },
+  {
+    name: "Leela",
+    age: 40,
+    plase: "BLR",
+    mobile: 776693843,
+  },
+  {
+    name: "Leela",
+    age: 40,
+    plase: "BLR",
+    mobile: 776693843,
+  },
+  {
+    name: "Leela",
+    age: 40,
+    plase: "BLR",
+    mobile: 776693843,
+  },
+  {
+    name: "Leela",
+    age: 40,
+    plase: "BLR",
+    mobile: 776693843,
+  },
+  {
+    name: "Leela",
+    age: 40,
+    plase: "BLR",
+    mobile: 776693843,
+  },
+  {
+    name: "Leela",
+    age: 40,
+    plase: "BLR",
+    mobile: 776693843,
+  },
+];
